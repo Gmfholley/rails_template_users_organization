46949d1bae7d98cb4bd4a2682df16ea151e30341
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-13 09:18:02 -0600

    It is the relationship between the role and user fixtures which is causing the error.  I do not know why.  Removed and tests are passing

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -7,13 +7,16 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "the truth" do
+    assert true
+  end
   
   test 'roles should not save without names' do
     a = Role.new
