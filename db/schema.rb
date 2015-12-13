74a47553fe653dc483b835d3de4520b50264324d
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-12 22:05:03 -0600

    Ran db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213034155) do
+ActiveRecord::Schema.define(version: 20151213040352) do
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "token"
   end
 
   create_table "roles", force: :cascade do |t|
