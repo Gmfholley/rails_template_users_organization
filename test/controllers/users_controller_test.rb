47c0461199109defb64a869964b777690bfbd1bf
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-13 19:56:57 -0600

    Starting to write some tests for users controllers

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,8 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  include Sorcery::TestHelpers::Rails::Integration
+  include Sorcery::TestHelpers::Rails::Controller
+  
 end
