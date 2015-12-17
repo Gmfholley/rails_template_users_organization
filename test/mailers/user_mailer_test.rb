7584e7be8b086353a9f9577ffcaff934899c7b92
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-16 20:34:15 -0600

    Rewrites the test to cover what I am actually doing

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,11 +2,10 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "reset_password_email" do
-    mail = UserMailer.reset_password_email
-    assert_equal "Reset password email", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = UserMailer.reset_password_email(users(:susan))
+    assert_equal "Your password has been reset", mail.subject
+    assert_equal [users(:susan).email], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
   end
 
 end
