2483f9e45f0a9f9e1ab0c02653b8ab934b862eb4
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-22 12:40:24 -0600

    adds a welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   
+  get 'welcome/index'
+
   post 'password_resets' => 'password_resets#create', as: :password_resets
   get 'password_resets/:id/edit' => 'password_resets#edit', as: :edit_password_resets
   put 'password_resets/:id' => 'password_resets#update', as: :password_reset
