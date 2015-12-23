c6d93852635618f77a7e210711e69bf8aac7722d
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-22 18:09:19 -0600

    Adds test if not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -26,11 +26,16 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to profile_path
   end
   
-  test "should show user profile page" do
+  test "should show user profile page if logged in" do
     @current_user = users(:susan)
     login_user(user = @current_user, route = login_path)
     get :show, id: @current_user.id
     assert_response :success
+  end
+  
+  test "should not show user profile page if not logged in" do
+    get :show, id: users(:susan).id
+    assert_redirected_to login_path
   end
   
 
