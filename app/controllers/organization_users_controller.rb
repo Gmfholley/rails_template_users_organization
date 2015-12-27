061107575faac27b16b436262bc2b74156365e7d
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-27 16:56:07 -0600

    Added controller actions.  I am not going to write in views.  This will be done with json

--- app/controllers/organization_users_controller.rb
+++ app/controllers/organization_users_controller.rb
@@ -1,17 +1,42 @@
 class OrganizationUsersController < ApplicationController
-  before_action :set_change_user
-  before_action :set_organization
-  before_action :has_authorization
   before_action :set_organization_user
+  before_action :has_authorization
      
   def create
+    if @organization_user.create
+      respond_to do |format|
+        format.json { render :json => @organization_user, status: :success }
+      end
+    else
+      respond_to do |format|
+        format.json { render :json => @organization_user.errors, status: :failure }
+      end
     
+    end
   end
   
   def update
+    if @organization_user.update(organization_user_params)
+      respond_to do |format|
+        format.json { render :json => @organization_user, status: :success }
+      end
+    else
+      respond_to do |format|
+        format.json { render :json => @organization_user.errors, status: :failure }
+      end
+    end
   end
   
   def destroy
+    if @organization_user.destroy
+      respond_to do |format|
+        format.json { render :nothing, status: :no_content }
+      end
+    else
+      respond_to do |format|
+        format.json { render :json => @organization_user.errors, status: :failure }
+      end
+    end
   end
   
   private
@@ -20,20 +45,39 @@ class OrganizationUsersController < ApplicationController
     @organization = Organization.find_by(token: params[:id])
   end
   
+  # sets the current user
+  # Note: if there is no params[:user_id], then the user to change is the current user
+  #
+  # returns the user to change
   def set_change_user
-    @change_user = Organization.find(params[:user_id])
+    if params[:user_id].blank?
+      @change_user = current_user
+    else
+      @change_user = Organization.find(params[:user_id])
+    end
   end
   
   def set_organization_user
+    set_change_user
+    set_organization
     @organization_user = OrganizationUser.find_by(user: @change_user, organization: @organization)
+  end
+  
+  def change_user_is_current_user?
+    @change_user == current_user
+  end
+  
+  # if not an admin or the user him/herself, redirects to not_authorized path
+  #
+  # returns nothing
+  def has_authorization
+    if !is_admin? || !change_user_is_current_user?
+      not_authorized
+    end
   end
   
   # Never trust parameters from the scary internet, only allow the white list through.
   def organization_user_params
     params.require(:organization_user).permit(:user_id, :organization_id, :role_id)
-  end
-      
-  def password_params
-     params["organization"]["users_attributes"]["0"]["password"]
   end
 end
