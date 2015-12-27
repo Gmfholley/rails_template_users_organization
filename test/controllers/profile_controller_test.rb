f9a193c9205eaaa24d31bc2e2e86c2e9dce8e7ec
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-27 16:15:50 -0600

    Debugs typo

--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ProfileControllerTest < ActionController::TestCase
 
-  test "should get edit" dow
+  test "should get edit" do
     get :edit
     assert_response :success
   end
