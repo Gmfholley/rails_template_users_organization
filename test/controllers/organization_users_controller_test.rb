8fe2987b5d03b7f100aa821c917a83a92c1c6073
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-27 22:56:51 -0600

    Created some more passing tests

--- test/controllers/organization_users_controller_test.rb
+++ test/controllers/organization_users_controller_test.rb
@@ -53,7 +53,7 @@ class OrganizationUsersControllerTest < ActionController::TestCase
       post :create, id: @organization_with_an_admin.token, user_id: @non_admin_user.id, organization_user: { organization_id: @organization_with_an_admin.id, user_id: @non_admin_user.id }
     end
     # david should not be able to create something for susan at the bank, since he is not 
-    assert_no_difference('OrganizationUser.count', 1) do
+    assert_no_difference('OrganizationUser.count') do
       post :create, id: @organization_without_an_admin.token, user_id: @admin_user.id, organization_user: { organization_id: @organization_without_an_admin.id, user_id: @admin_user.id }
     end
     
@@ -64,21 +64,61 @@ class OrganizationUsersControllerTest < ActionController::TestCase
   
   test "should get update if user is logged in and authorized" do
     login_user(user = @admin_user, route = login_path) 
-    
+    # admin should be able to change their role to an admin
     put :update, id: @organization_with_an_admin.token, user_id: @admin_user.id, organization_user: { organization_id: @organization_with_an_admin.id, user_id: @admin_user.id, role_id: roles(:admin).id }
     assert_equal assigns(:organization_user).role.id, roles(:admin).id
-
+    
+    # admin should be able to change their role to a user
     put :update, id: @organization_with_an_admin.token, user_id: @admin_user.id, organization_user: { organization_id: @organization_with_an_admin.id, user_id: @admin_user.id, role_id: roles(:user).id }
     assert_equal assigns(:organization_user).role.id, roles(:user).id
 
     assert_redirected_to :back    
   end
   
+  test "should not update if user is not authorized" do
+    login_user(user = @non_admin_user, route = login_path) 
+    # non admin should not be able to change their role to an admin
+    put :update, id: @organization_without_an_admin.token, user_id: @non_admin_user.id, organization_user: { organization_id: @organization_without_an_admin.id, user_id: @non_admin_user.id, role_id: roles(:admin).id }
+    
+    
+    #but david, as a non-admin, should be redirected with not authorized message
+    assert_redirected_to profile_path, "Not authorized but allowed to go"
+    assert_equal flash[:alert], "You do not have access to view that page.", "Not the right alert"  
+
+    # and assert that the role didn't change
+    assert_equal @non_admin_user.role(@organization_without_an_admin), roles(:user).id
+  end
+  
   test "should get destroy if user is logged in" do 
     login_user(user = @admin_user, route = login_path) 
     
+    # should be able to create and destroy a user
+    assert_difference('OrganizationUser.count', 1) do
+      post :create, id: @organization_with_an_admin.token, user_id: @non_admin_user.id, organization_user: { organization_id: @organization_with_an_admin.id, user_id: @non_admin_user.id }
+    end
+    # should be able to destroy new user if an admin
+    assert_difference('OrganizationUser.count', -1) do
+      delete :destroy, id: @organization_with_an_admin.token, user_id: @non_admin_user.id, organization_user: { organization_id: @organization_with_an_admin.id, user_id: @non_admin_user.id }
+    end
+        
+    # should be able to destroy him/herself
     assert_difference('OrganizationUser.count', -1) do
       delete :destroy, id: @organization_with_an_admin.token, user_id: @admin_user.id, organization_user: { organization_id: @organization_with_an_admin.id, user_id: @admin_user.id }
+    end
+    assert_redirected_to :back    
+  end
+  
+  test "should not destroy if user is not authorized" do 
+    login_user(user = @non_admin_user, route = login_path) 
+    
+    # should not be able to destroy another party
+    assert_no_difference('OrganizationUser.count') do
+      delete :destroy, id: @organization_with_an_admin.token, user_id: @admin_user.id, organization_user: { organization_id: @organization_with_an_admin.id, user_id: @admin_user.id }
+    end
+    
+    # should be able to destroy him/herself
+    assert_difference('OrganizationUser.count', -1) do
+      delete :destroy, id: @organization_without_an_admin.token, user_id: @non_admin_user.id, organization_user: { organization_id: @organization_without_an_admin.id, user_id: @non_admin_user.id }
     end
     assert_redirected_to :back    
   end
